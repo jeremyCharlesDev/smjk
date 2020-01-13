59edaa5729608605605c4279fa2175a3cfdf2880
MorinKevin <morin-kevin@outlook.fr>
2020-01-13 16:00:28 +0100

    deuxieme commit

---
+++ View/ProduitView.php
@@ -0,0 +1,16 @@
+<?php
+class ClientView extends View
+{
+    
+    //######################################################
+    /**
+     * Affichage de la liste
+     *
+     * @return void
+     ******************************************************/
+    public function displayHome()
+    {
+        $this->displayPage();
+    }
+    
+}
